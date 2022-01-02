0cd9fa93125086a14e79165e1826dfa97e54b2c9
vinay-winai <60219349+vinay-winai@users.noreply.github.com>
2022-01-02 15:07:09 +0530

    Update serial_monitor.py

--- serial_monitor.py
+++ serial_monitor.py
@@ -3,6 +3,6 @@ import serial
 serialport = serial.Serial('COM4',baudrate=115200,timeout=2)
 
 for i in range(1,13):
-    arduinodata = serialport.readline()
+    arduinodata = serialport.readline().decode('ascii')
     print(arduinodata)
 
