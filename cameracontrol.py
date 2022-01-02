570e6c0632a3a8a12548ad438749c4d16612b2e9
vinay-winai <60219349+vinay-winai@users.noreply.github.com>
2022-01-02 15:21:20 +0530

    Update cameracontrol.py

--- cameracontrol.py
+++ cameracontrol.py
@@ -1,26 +1,32 @@
 # import the opencv library
 import cv2
+import serial
 
+serialport = serial.Serial('COM4',baudrate=115200,timeout=2)
+
+for i in range(1,13):
+    arduinodata = serialport.readline().decode('ascii')
+    print(arduinodata)
 
 # define a video capture object
-vid = cv2.VideoCapture(0)
-
-while(True):
-	
-	# Capture the video frame
-	# by frame
-	ret, frame = vid.read()
-
-	# Display the resulting frame
-	cv2.imshow('frame', frame)
-	
-	# the 'q' button is set as the
-	# quitting button you may use any
-	# desired button of your choice
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-
-# After the loop release the cap object
-vid.release()
+if(arduinodata):
+    vid = cv2.VideoCapture(0)
+  
+    while(True):
+      
+    # Capture the video frame
+    # by frame
+        ret, frame = vid.read()
+  
+    # Display the resulting frame
+        cv2.imshow('frame', frame)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+	# After the loop release the cap object
+    vid.release()
 # Destroy all the windows
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
