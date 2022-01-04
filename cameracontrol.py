ce98d6856c19838242cd43279f74c0e55641e32c
sandeep <83683801+gsandeepreddy412@users.noreply.github.com>
2022-01-04 18:54:42 +0530

    Update cameracontrol.py

--- cameracontrol.py
+++ cameracontrol.py
@@ -1,7 +1,7 @@
 # import the opencv library
 import cv2
 import serial
-
+import time
 serialport = serial.Serial('COM4',baudrate=115200,timeout=2)
 for i in range(0,11):
     arduinodata = serialport.readline()
@@ -9,22 +9,14 @@ for i in range(0,11):
 
 # define a video capture object
 if(arduinodata == b'1\r\n'):
+    capture_duration = 60
+    start_time = time.time() # records current time
     vid = cv2.VideoCapture(0)
-  
-    while(True):
-      
-    # Capture the video frame
-    # by frame
+    while(int(time.time() - start_time) < capture_duration ):
+    # Capture the video frame by frame
         ret, frame = vid.read()
-  
     # Display the resulting frame
         cv2.imshow('frame', frame)
-      
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-        if :
-            break
 	# After the loop release the cap object
     vid.release()
 # Destroy all the windows
