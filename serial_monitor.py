a62de95029a42c5828fcdb8ee36644d5daa1b30a
vinay-winai <60219349+vinay-winai@users.noreply.github.com>
2022-08-11 11:01:03 +0530

    Delete serial_monitor.py

--- serial_monitor.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-serialport = serial.Serial('COM4',baudrate=115200,timeout=2)
-
-for i in range(1,13):
-    arduinodata = serialport.readline().decode('ascii')
-    print(arduinodata)
-
